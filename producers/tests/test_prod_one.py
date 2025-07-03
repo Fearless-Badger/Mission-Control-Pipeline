afe421f6ffe5de89bae74fcb1e7ac58945fd8fae
Fearless-Badger <micah.najacht@gmail.com>
2025-07-02 23:08:27 -0500

    Add healthcheck to container

--- producers/tests/test_prod_one.py
+++ producers/tests/test_prod_one.py
@@ -3,8 +3,8 @@ from producers.rocket_data_gen import app
 
 test_producer = TestClient(app)
 
+
 def test_ping():
     response = test_producer.get("/ping")
     assert response.status_code == 200
     assert response.json() == {"ping": "pong"}
-    
