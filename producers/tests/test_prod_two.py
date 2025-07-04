06282ea53bfdb5c34c632d8f81daf12498cffa0c
Fearless-Badger <micah.najacht@gmail.com>
2025-07-04 17:18:01 -0500

    Add second producer's skeleton

--- producers/tests/test_prod_two.py
+++ producers/tests/test_prod_two.py
@@ -1,12 +1,9 @@
 from fastapi.testclient import TestClient
-from producers.rocket_data_gen import app
+from producers.onboard_info_gen import app
 
 test_producer = TestClient(app)
 
-
-def test_ping():
+def test_health():
     response = test_producer.get("/ping")
     assert response.status_code == 200
     assert response.json() == {"ping": "pong"}
-    assert True
-    
